4078705f9b94406e08c0ea6c558f27ec72b5d45a
camilo contreras <camilo.coo250@gmail.com>
2019-09-22 16:55:19 -0500

    title created with styled components

---
+++ src/components/Home/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import StyledTitle from '../StyledTitle'
+
+const About = () => {
+    return (
+        <div>
+            Hello from about 
+            <StyledTitle title="about" subtitle="us"/>
+        </div>
+    )
+}
+
+export default About
---
+++ src/examples/Button.js
@@ -0,0 +1,11 @@
+import styled  from 'styled-components'
+
+
+const Button = styled.button`
+    color:red;
+    background-color:green;
+    font-size:3rem;
+
+`
+
+export default Button 
