24c644f92e163e2b14928d17566755a684ff9838
camilo contreras <camilo.coo250@gmail.com>
2019-09-22 17:10:00 -0500

    about section done

--- src/components/Home/About.js
+++ src/components/Home/About.js
@@ -1,11 +1,27 @@
 import React from 'react'
 import StyledTitle from '../StyledTitle'
+import styles from '../../css/about.module.css'
+import img from '../../images/defaultBcg.jpeg'
+
 
 const About = () => {
     return (
-        <div>
+        <div className={styles.about}>
             Hello from about 
             <StyledTitle title="about" subtitle="us"/>
+            <div className={styles.aboutCenter}>
+                <article className={styles.aboutImg}>
+                    <div className={styles.imgContainer}>
+                        <img src={img} alt="about company"/>
+                    </div>
+                </article>
+                <article className={styles.aboutInfo}>
+                    <h4>Explore the differences</h4>
+                    <p>this are the difference from unique parts around the world</p>
+                    <p>look at the  birds, look the trees , look around and hold the sight on the that world that is yours</p>
+
+                </article>
+            </div>
         </div>
     )
 }
--- src/components/StyledTitle.js
+++ src/components/StyledTitle.js
@@ -2,7 +2,7 @@ import React from 'react'
 import styled from "styled-components"
 
 
-const StyledTitle = ({title,subtitle}) => {
+const StyledTitle = ({title,subtitle }) => {
     return (
         <TitleWrapper>
             <h4>
