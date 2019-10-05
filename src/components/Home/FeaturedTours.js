fb94f976610ef9bbdced449ba31a61f6f71593fb
camilo contreras <camilo.coo250@gmail.com>
2019-10-05 16:37:46 -0500

    data feteched

--- src/components/Home/FeaturedTours.js
+++ src/components/Home/FeaturedTours.js
@@ -1,12 +1,58 @@
 import React from 'react'
 import Tour from '../../Tours/Tour'
+import {graphql,useStaticQuery} from 'gatsby'
+import StyledTitle from '../StyledTitle'
+import styles from '../../css/items.module.css'
+import AniLink from 'gatsby-plugin-transition-link'
+
+const getFeaturedTours = graphql`
+{
+    featuredTours:allContentfulTour(filter:{featured:{eq:true}}){
+      edges{
+        node{
+            name
+          price
+          slug
+          country
+          contentful_id
+          days
+          images{
+            fluid{
+              ...GatsbyContentfulFluid_tracedSVG
+            }
+          }
+        }
+      }
+    }
+  }
+
+
+
+`
+
+
 
 const FeaturedTours = () => {
+    const response = useStaticQuery(getFeaturedTours);
+    const {featuredTours:{edges:arrayOfTours}} = response;
+    console.log(arrayOfTours)
+    
     return (
-        <div>
+        <section className={styles.tours}>
+            <StyledTitle title="featured" subtitle="tours"/>
+            {arrayOfTours.map(({node:tour})=>{
+                return(
+                    <Tour key={tour.contentful_id} tour={tour}/>   
+                )
+            })}
             hello from featured tours 
-            <Tour/>
-        </div>
+
+           
+            <AniLink to="/tours" className="btn-primary">
+                all tours
+            </AniLink>
+        
+        </section>
     )
 }
 
